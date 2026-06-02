c9b3e1f0aa42314636245fe43fbf36e83705a8fd
hvvup <hvvijung@gmail.com>
2026-06-02 14:42:30 +0900

    [CHORE]: port 번호 수정

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -28,15 +28,15 @@ resource "aws_security_group" "rds" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 3306
-    to_port         = 3306
+    from_port       = 5432
+    to_port         = 5432
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id, aws_security_group.bastion.id]
   }
 
   egress {
-    from_port       = 3306
-    to_port         = 3306
+    from_port       = 5432
+    to_port         = 5432
     protocol        = "tcp"
     security_groups = ["0.0.0.0/0"]
   }
