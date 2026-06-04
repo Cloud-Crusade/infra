51e800022823ce4466fb6bbe8976207bdd114a66
yuhyunsang06 <delipure@naver.com>
2026-06-04 16:37:29 +0900

    [FEAT]: backend.tf파일에서 key값 변경

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-bucket-d8f5bb8d"
-    key            = "tfstate/terraform.tfstate"
+    key            = "dev/terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "terraform-lock"
     encrypt        = true
