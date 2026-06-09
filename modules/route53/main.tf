300351fd2603c3b4ebaa859e806e2e2c97f8e410
yuhyunsang06 <delipure@naver.com>
2026-06-09 18:19:50 +0900

    [REFAC]: terraform fmt 명령어 적용

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "www" {
 
   alias {
     name                   = var.cloudfront_domain_name
-    zone_id                = var.cloudfront_zone_id 
+    zone_id                = var.cloudfront_zone_id
     evaluate_target_health = false
   }
 }
