ec8aa7dc63f38ed39ae417809bc07c16b430bc9a
yuhyunsang06 <delipure@naver.com>
2026-06-09 17:34:12 +0900

    [REFAC]: ACM 블록 제거 및 root 도메인 추가

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,37 +1,18 @@
-# 1. ACM 인증서 발급
-resource "aws_acm_certificate" "cert" {
-  domain_name               = "*.${var.domain_name}"
-  validation_method         = "DNS"
-  subject_alternative_names = [var.domain_name]
 
-  lifecycle { create_before_destroy = true }
-}
+# einsof.app (Root 도메인) -> CloudFront 매핑
+resource "aws_route53_record" "root" {
+  zone_id = var.route53_zone_id
+  name    = var.domain_name # 👈 "www." 없이 변수명만 지정하면 루트 도메인이 됩니다.
+  type    = "A"
 
-# 2. DNS 검증용 레코드 자동 생성
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
+  alias {
+    name                   = var.cloudfront_domain_name
+    zone_id                = var.cloudfront_zone_id
+    evaluate_target_health = false
   }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = var.route53_zone_id
 }
 
-# 3. 인증서 검증 대기 완료
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
-
-# 4. api.einsof.app -> ALB 매핑
+# api.einsof.app -> ALB 매핑
 resource "aws_route53_record" "api" {
   zone_id = var.route53_zone_id
   name    = "api.${var.domain_name}"
@@ -44,7 +25,7 @@ resource "aws_route53_record" "api" {
   }
 }
 
-# 5. www.einsof.app -> CloudFront 매핑
+# www.einsof.app -> CloudFront 매핑
 resource "aws_route53_record" "www" {
   zone_id = var.route53_zone_id
   name    = "www.${var.domain_name}"
@@ -52,7 +33,7 @@ resource "aws_route53_record" "www" {
 
   alias {
     name                   = var.cloudfront_domain_name
-    zone_id                = var.cloudfront_zone_id # 👈 하드코딩 주소에서 변수 참조로 변경!
+    zone_id                = var.cloudfront_zone_id 
     evaluate_target_health = false
   }
 }
