f9ef7d2bf36b89a5c7a316d460c9ecbf406b5141
yuhyunsang06 <delipure@naver.com>
2026-06-09 17:50:07 +0900

    [REFAC]: cloudfront에 root 도메인 추가

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,18 +1,22 @@
 
-# einsof.app (Root 도메인) -> CloudFront 매핑
-resource "aws_route53_record" "root" {
+resource "aws_route53_record" "cloudfront_domains" {
+  for_each = toset([
+    var.domain_name,          
+    "www.${var.domain_name}"  
+  ])
+
   zone_id = var.route53_zone_id
-  name    = var.domain_name # 👈 "www." 없이 변수명만 지정하면 루트 도메인이 됩니다.
+  name    = each.value
   type    = "A"
 
   alias {
     name                   = var.cloudfront_domain_name
-    zone_id                = var.cloudfront_zone_id
+    zone_id                = var.cloudfront_zone_id 
     evaluate_target_health = false
   }
 }
 
-# api.einsof.app -> ALB 매핑
+# 2. api.einsof.app -> ALB 매핑
 resource "aws_route53_record" "api" {
   zone_id = var.route53_zone_id
   name    = "api.${var.domain_name}"
@@ -22,18 +26,5 @@ resource "aws_route53_record" "api" {
     name                   = var.alb_dns_name
     zone_id                = var.alb_zone_id
     evaluate_target_health = true
-  }
-}
-
-# www.einsof.app -> CloudFront 매핑
-resource "aws_route53_record" "www" {
-  zone_id = var.route53_zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = var.cloudfront_domain_name
-    zone_id                = var.cloudfront_zone_id
-    evaluate_target_health = false
   }
 }
