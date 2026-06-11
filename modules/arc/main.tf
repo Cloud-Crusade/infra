b4dd3f8f787cf9c90e7776352c6490bb70ceb3d1
hvvup <hvvijung@gmail.com>
2026-06-11 16:23:34 +0900

    [FEAT]: ARC 모듈 구현(테스트 전)

---
+++ modules/arc/main.tf
@@ -0,0 +1,17 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_arczonalshift_zonal_autoshift_configuration" "this" {
+    resource_arn = var.lb_arn
+    zonal_autoshift_status = var.zonal_autoshift_status
+    
+    dynamic "outcome_alarms" {
+        for_each = var.outcome_alarms
+
+        content {
+            type = outcome_alarms.value.type
+            alarm_identifier = outcome_alarms.value.alarm_identifier
+        }
+    }
+}
