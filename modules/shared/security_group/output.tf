4bccbdea84e8ccef81dfa56d9c553d7a7a37f6fc
hvvup <hvvijung@gmail.com>
2026-06-18 14:46:51 +0900

    [FEAT]: RDS Proxy 보안 그룹 추가 및 RDS SG 인바운드 정책 변경

--- modules/shared/security_group/output.tf
+++ modules/shared/security_group/output.tf
@@ -6,6 +6,10 @@ output "rds_sg_id" {
   value = aws_security_group.rds.id
 }
 
+output "rds_proxy_sg_id" {
+  value = aws_security_group.rds_proxy.id
+}
+
 output "eks_sg_id" {
   value = aws_security_group.eks.id
 }
